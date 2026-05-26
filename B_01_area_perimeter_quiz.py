e4cf85121402baf885e4c6ade82dcc63590c8a2d
Lemonade-aaa <julianr4258@masseyhigh.school.nz>
2026-05-26 13:34:40 +1200

    full quiz

--- B_01_area_perimeter_quiz.py
+++ B_01_area_perimeter_quiz.py
@@ -1,4 +1,4 @@
-
+import random
 
 
 # checks user enters yes (y) or no (n)
@@ -25,6 +25,7 @@ def string_checker(question, valid_ans=('yes', 'no')):
         print(error)
         print()
 
+
 def instructions():
     """Print instructions"""
 
@@ -32,17 +33,21 @@ def instructions():
 ***INSTRUCTIONS***
 
 Do math
-you will be given the width and height, calculate the perimeter and area.
+you will be given random width and height, 
+use them to calculate the perimeter and area.
+All shapes are rectangles/squares for perimeter/area.
+use code 'xxx' to exit the quiz at anytime.
+you can choose to see the end results and summary at the end.
+
 Area calc = width x height.
 Perimeter calc = height + width x 2.
 
 Have fun :)
-
     """)
 
 
-
-
+print("\n➕✖️🟰Welcome to MY perimeter area quiz🟰✖️➕\n")
+print()
 
 
 want_instructions = string_checker("Do you wish to see the instructions? ").lower()
@@ -50,4 +55,224 @@ want_instructions = string_checker("Do you wish to see the instructions? ").lowe
 #checks users enter yes (y) or no (n)
 if want_instructions == "yes":
     instructions()
+
+
+# checks for an integer with optional upper /
+# lower limits and optional exit code for infinite mode
+# / quitting the game
+def int_check(question, low=None, high=None, exit_code=None,):
+
+
+    # if any integer is allowed...
+    if low is None and high is None:
+        error = "please enter an integer"
+
+    # if larger number needs to be more than an
+    # integer (ie: rounds / 'high number')
+    elif low is not None and high is None:
+        error = (f"Please enter and integer that is "
+                 f"more than / equal to {low}")
+
+    # if the number needs to be between low & high
+    else:
+        error = (f"Please enter a integer that "
+                 f"is between {low} and {high} (inclusive)")
+
+    while True:
+        response = input(question).lower()
+
+        # check for infinite mode
+        if response == "I".lower():
+            return "infinite"
+
+        #check for infinite mode / exit code
+        if response == exit_code:
+            return response
+
+        try:
+            response = int(response)
+
+            #check the integer is not too low
+            if low is not None and response < low:
+                print(error)
+
+            #check response is more than low number
+            elif high is not None and response > high:
+                print(error)
+
+            #if response is valid, return it
+            else:
+                return response
+
+        except ValueError:
+            print(error)
+
+
+area_quest = "false"
+perim_quest = "false"
+mode = "regular"
+quest_answered = 0
+quest_wrong = 0
+quest_right = 0
+quiz_results = []
+right_wrong = ""
+
+# Ask the user for number of questions / infinite mode
+num_questions = int_check("How many questions would you like?"
+                          " ('i' for infinite): ",
+                          low=1)
+
+if num_questions == "infinite":
+    mode = "infinite"
+    num_questions = 5
+
+
+# ask user if they want to customise the number range
+default_params = string_checker("Do you want the default height and width (this will pick a random number between 1 & 10)? ")
+if default_params == "yes":
+    low_num = 1
+    high_num = 10
+
+# Allow user to choose the high / low number
+else:
+    low_num = int_check("What would you like as the minimum? ")
+    high_num = int_check("what would you like as the maximum? ", low=low_num + 1)
+
+# Game loop starts here
+while quest_answered < num_questions:
+
+    # Rounds headings (based on mode)
+    if mode == "infinite":
+        round_heading = f"\n♾️♾️♾️ Question {quest_answered + 1} (Infinite mode) ♾️♾️♾️"
+    else:
+        round_heading = f"\n3️⃣2️⃣1️⃣ Question {quest_answered + 1} of {num_questions} 1️⃣2️⃣3️⃣"
+
+    print(round_heading)
+
+    #determines the type of question asked
+    quest_type = random.randint(1, 2)
+
+    # generate numbers for question
+    width = random.randint(low_num, high_num)
+    height = random.randint(low_num, high_num)
+
+    # calculate answers
+    area = width * height
+    perimeter = 2 * (width + height)
+
+
+    #sorts type of question given
+    if quest_type == 1:
+        area_quest = "true"
+        perim_quest = "false"
+    elif quest_type == 2:
+        perim_quest = "true"
+        area_quest = "false"
+
+    # #testing remove when done
+    # print("area", area)
+    # print("perimeter", perimeter)
+
+    # makes infinite mode infinite
+    if mode == "infinite":
+        num_questions += 1
+
+
+    # determine type of question and whether it is correct
+    if area_quest == "true":
+
+        #asks question,
+        user_ans = (int_check(f"\nif the width is {width} and the height is {height}"
+                        f" What is the area? ", exit_code="xxx"))
+
+        # checks if user is correct
+        if user_ans == area:
+            quest_right += 1
+            #determines if question is right
+            right_wrong = "right"
+
+        #allows user to exit quiz
+        elif user_ans == "xxx":
+            print("\nYou left :(")
+            break
+
+        #checks if user is wrong
+        else:
+            quest_wrong += 1
+            #determines if question is wrong
+            right_wrong = "wrong"
+
+        #gets result of question
+        quest_results = f" At question {quest_answered + 1} you answered {user_ans} the answer was {area} you were {right_wrong}"
+
+        # make questions progress
+        quest_answered += 1
+
+        quiz_results.append(quest_results)
+
+    # determine type of question and whether it is correct
+    elif perim_quest == "true":
+
+        user_ans = (int_check(f"\nif the width is {width} and the height is {height}"
+                        f" What is the perimeter? ", exit_code="xxx"))
+
+        # checks if user is correct
+        if user_ans == perimeter:
+            quest_right += 1
+            # determines if question is right
+            right_wrong = "right"
+
+        #allows user to exit quiz
+        elif user_ans == "xxx":
+            print("\nYou left :(")
+            break
+
+
+        #checks if user is wrong
+        else:
+            quest_wrong += 1
+            # determines if question is wrong
+            right_wrong = "wrong"
+
+        #gets result of question
+        quest_results = f" At question {quest_answered + 1} you answered {user_ans} the answer was {perimeter} you were {right_wrong}"
+
+        # make questions progress
+        quest_answered += 1
+
+        quiz_results.append(quest_results)
+
+print("\nAll done pal (。・ω・。)\n")
+
+#check is user wants to see quiz history
+want_end_result = string_checker("❔❓❔Would you like to see how you did?❓❔❓ ")
+
+if want_end_result == "yes":
+
+    print("\n✨✨Quiz Results✨✨\n")
+
+    for item in quiz_results:
+        print(item)
+
+    #calculates variables for summary
+    percent_wrong = quest_wrong / quest_answered * 100
+    percent_right = quest_right / quest_answered * 100
+    average_percent = quest_wrong + quest_right / quest_answered * 100
+
+    print(f"\nSummary: You got {quest_right} questions right"
+          f" and {quest_wrong} questions wrong. "
+          f"Percentages: Wrong {percent_wrong:.2f} Right: {percent_right:.2f} Average: {average_percent:.2f}\n")
+
+
+    # tells user whether they were right/wrong for majority
+    if percent_right < percent_wrong:
+        print("You were wrong for the majority")
+
+    elif percent_wrong < percent_right:
+        print("You were right for the majority")
+
+    elif percent_wrong == percent_right:
+        print("Your as GREAT as you are BAD!")
+
+print("\nHave a lovely day!")
 
